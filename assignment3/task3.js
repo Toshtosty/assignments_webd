20599738efb222b04c969acd4acd48ed47e8bad1
toshtoasty <toshitagarg1234@gmail.com>
2025-05-16 12:37:27 +0530

    task3 done

--- assignment3/task3.js
+++ assignment3/task3.js
@@ -29,3 +29,79 @@ Add console logs to indicate the start and completion of each simulated step.
 
 
 */
+
+console.log("task 3 with async await");
+
+
+function downloadData(url) {
+    console.log(`download started ${url}...`);
+    return new Promise((resolve, reject) => {
+
+
+        setTimeout(() => {
+            const data = "any data from the given url"
+            console.log(`download completed ${url}...`);
+
+            resolve(data);
+
+        }, 2000);
+
+    })
+
+
+
+}
+
+function GenerateFile(data) {
+
+    console.log("generating file....");
+    return new Promise((resolve, reject) => {
+
+
+        setTimeout(() => {
+
+            const file = "file_1.pdf";
+            console.log(`file created , file name :-${file}....`);
+            // let url = "www.fhijfh.com"
+
+            resolve(file);
+            //return file_xyz;
+
+        }, 2000)
+
+    })
+
+
+
+
+}
+
+
+function uploadFile(file, up_url) {
+    console.log(`uploading of ${file} to ${up_url} started`);
+
+    return new Promise((resolve, reject) => {
+
+
+        setTimeout(() => {
+            console.log(`file ${file} uploaded successfully on ${up_url} `);
+            resolve(up_url);
+        }, 2000)
+
+    })
+}
+
+
+(async () => {
+
+    console.log("process starts");
+    
+
+    let data= await downloadData("www.downloadUrl.com");
+    let file= await GenerateFile(data);
+    let final_url= await uploadFile(file,"www.uploadUrl.com");
+
+    console.log("process all done!!");
+    
+
+})();
